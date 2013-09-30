124a1bb3b76f0fce02d6eb921dc227cb1c26e055
Howard Chen <howardxchen@gmail.com>
2013-09-30 12:39:13 -0700

    Fix style, remove unecessary stuff in route.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
-	validates(:first_name, presence: true, length: { minimum: 1 })
-	validates(:last_name, presence: true, length: { minimum: 1 })
-	validates_inclusion_of :age, in: 0..99
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                   	  uniqueness: { case_sensitive: false }
-    has_secure_password
-    validates :password, length: { minimum: 6 }
+  has_secure_password
+
+  before_save { email.downcase! }
+
+  validates :first_name, presence: true, length: { minimum: 1 }
+  validates :last_name, presence: true, length: { minimum: 1 }
+  validates_inclusion_of :age, in: 0..99
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+             uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 USOMC::Application.routes.draw do
   root :to => 'static_pages#home'
+<<<<<<< HEAD
   match 'pieces', to: 'pieces#index', via: :get
   get "users/new"
+=======
+  match 'users/new', to: 'users#new', via: :get
+>>>>>>> Fix style, remove unecessary stuff in route.rb
 end
