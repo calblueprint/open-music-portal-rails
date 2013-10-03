e2ca3872638a8430bcdd6decae8040403a86ad5e
Howard Chen <howardxchen@gmail.com>
2013-10-03 04:57:56 -0700

    Modify config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 USOMC::Application.routes.draw do
   root :to => 'static_pages#home'
-<<<<<<< HEAD
   match 'pieces', to: 'pieces#index', via: :get
-  get "users/new"
-=======
   match 'users/new', to: 'users#new', via: :get
->>>>>>> Fix style, remove unecessary stuff in route.rb
 end
