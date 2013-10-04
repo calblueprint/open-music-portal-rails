31db6c6c32ea097dc2efd46d03b57e40dce6c923
Mark Miyashita <negativetwelve@gmail.com>
2013-10-03 18:58:03 -0700

    Added success/error tag

--- app/models/response.rb
+++ app/models/response.rb
@@ -9,27 +9,27 @@ class Response < ActiveRecord::Base
   end
 
   def self.ok(message='')
-    self.new(code: 1, message: message, status: 200).to_json
+    self.new(code: 1, message: message, status: 200, success: true).to_json
   end
 
   def self.unauthorized(message='')
-    self.new(code: 2, message: message, status: 401).to_json
+    self.new(code: 2, message: message, status: 401, success: false).to_json
   end
 
   def self.bad_request(message='')
-    self.new(code: 3, message: message, status: 400).to_json
+    self.new(code: 3, message: message, status: 400, success: false).to_json
   end
 
   def self.forbidden(message='')
-    self.new(code: 4, message: message, status: 403).to_json
+    self.new(code: 4, message: message, status: 403, success: false).to_json
   end
 
   def self.not_found(message='')
-    self.new(code: 5, message: message, status: 404).to_json
+    self.new(code: 5, message: message, status: 404, success: false).to_json
   end
 
   def self.unprocessable(message='')
-    self.new(code: 6, message: message, status: 422).to_json
+    self.new(code: 6, message: message, status: 422, success: false).to_json
   end
 
 end
