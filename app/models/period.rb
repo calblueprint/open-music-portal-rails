d90f5bf9e211848de5a1caa6daf971aeefbbc5cd
Howard Chen <howardxchen@gmail.com>
2013-10-05 11:47:22 -0700

    Annotate Models

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: periods
+#
+#  id         :integer          not null, primary key
+#  name       :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Period < ActiveRecord::Base
   has_many :pieces
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  first_name      :string(255)
+#  last_name       :string(255)
+#  id_number       :integer
+#  date_of_birth   :date
+#  email           :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#  password_digest :string(255)
+#
+
 class User < ActiveRecord::Base
   has_secure_password
 
