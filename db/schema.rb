04911d189688d13ba2a5d6753a7be49703c4f64b
Howard Chen <howardxchen@gmail.com>
2013-10-08 00:17:35 -0700

    Separate updates to user in new migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130928234517) do
+ActiveRecord::Schema.define(version: 20131008070441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,15 +77,15 @@ ActiveRecord::Schema.define(version: 20130928234517) do
     t.integer  "id_number"
     t.date     "date_of_birth"
     t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
     t.string   "country"
     t.string   "street_address"
     t.string   "city"
     t.string   "state"
     t.string   "zip_code"
     t.string   "phone_number"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
