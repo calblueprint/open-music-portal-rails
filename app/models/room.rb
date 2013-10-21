f33945fa7874b9db6512335269ffdcf71cd6495a
Howard Chen <howardxchen@gmail.com>
2013-10-21 16:54:51 -0700

    Updated API

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,14 @@
 class Room < ActiveRecord::Base
-	has_many :users
-	validates :name, presence:true
+  has_many :users
+  validates :name, presence:true
+
+  def self.to_json(rooms)
+      return rooms.collect {|room| room.to_json}
+    end
+
+    def to_json
+      return {
+        name: name
+      }
+    end
 end
