2f49bfa71ea37bb536219b7b09fc2d04ca226ae3
chenhoward <howardxchen@gmail.com>
2013-10-27 21:18:46 -0700

    Start room model/create basic migration.  Need to know how rooms with interact with other models(with users or events?) before continuing.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,14 +1,13 @@
 class Room < ActiveRecord::Base
-  has_many :users
   validates :name, presence:true
 
   def self.to_json(rooms)
-      return rooms.collect {|room| room.to_json}
-    end
+    return rooms.collect {|room| room.to_json}
+  end
 
-    def to_json
-      return {
-        name: name
-      }
-    end
+  def to_json
+    return {
+      name: name
+    }
+  end
 end
