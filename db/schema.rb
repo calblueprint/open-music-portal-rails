c0216d3503399f645f3e8adc72685c25ddf64b38
Mark Miyashita <negativetwelve@gmail.com>
2013-10-28 16:50:03 -0700

    bumping new schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20131028224531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "admins", force: true do |t|
     t.string   "name"
