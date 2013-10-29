a1d0edb43a957a181ebe9092abf6c9bbd9f85b3e
Mark Miyashita <negativetwelve@gmail.com>
2013-10-28 20:19:52 -0700

    adding routes for new static pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ USOMC::Application.routes.draw do
 
   match 'profile', to: 'static_pages#profile', via: :get
   match 'registration', to: 'static_pages#registration', via: :get
+  match 'about', to: 'static_pages#about', via: :get
+  match 'competition', to: 'static_pages#competition', via: :get
+  match 'winners', to: 'static_pages#winners', via: :get
+  match 'judges', to: 'static_pages#judges', via: :get
+  match 'support', to: 'static_pages#support', via: :get
 
   match 'pieces/typeahead_search', to: 'pieces#typeahead_search', via: :get
   resources :pieces
