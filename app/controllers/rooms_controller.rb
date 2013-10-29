6f549c4188a99e3d2c3d4f9d186c378c27705990
chenhoward <howardxchen@gmail.com>
2013-10-28 21:05:50 -0700

    Create show, rooms now actually save

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,4 +6,23 @@ class RoomsController < ApplicationController
   def index
     @rooms = Room.all
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to @room
+    else
+      render 'new';
+    end
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  private
+
+    def room_params
+      params.require(:room).permit(:name)
+    end
 end
