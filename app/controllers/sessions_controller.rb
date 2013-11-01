c19dfd84b767e998587252f3e0c845e9aa2f717e
Mark Miyashita <negativetwelve@gmail.com>
2013-10-31 18:07:15 -0700

    Adding error/success messages back, minor fixes for sign in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      flash[:success] = "Welcome back to USOMC, #{user.first_name}!"
+      redirect_to profile_path
     else
       flash.now[:error] = 'Invalid email and password combination'
       render 'new'
