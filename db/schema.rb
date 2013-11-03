8848b2e2d9e51bfdc8b5854a7a3d44b50ced4050
Mark Miyashita <negativetwelve@gmail.com>
2013-11-03 12:08:36 -0800

    bumping schema with latest changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131102205104) do
+ActiveRecord::Schema.define(version: 20131103071122) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "admins", force: true do |t|
     t.string   "name"
@@ -38,6 +42,12 @@ ActiveRecord::Schema.define(version: 20131102205104) do
     t.datetime "updated_at"
   end
 
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "composers", force: true do |t|
     t.text     "name"
     t.datetime "created_at"
@@ -59,9 +69,10 @@ ActiveRecord::Schema.define(version: 20131102205104) do
     t.text     "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
-  create_table "events_pieces", id: false, force: true do |t|
+  create_table "events_pieces", force: true do |t|
     t.integer "event_id", null: false
     t.integer "piece_id", null: false
   end
