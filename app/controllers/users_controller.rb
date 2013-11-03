4ed4d4d38b205b47f4e774a61e04a5c946753257
chenhoward <howardxchen@gmail.com>
2013-11-02 23:39:50 -0700

    Remove redundancy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.add_role :contestant
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the USOMC!"
