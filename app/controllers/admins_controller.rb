6bdace63d250be6dc708e4a69fb2a29836cf8402
chenhoward <howardxchen@gmail.com>
2013-11-02 18:28:49 -0700

    Change to 10 per page, gave it order.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,7 +5,7 @@ class AdminsController < ApplicationController
   end
 
   def users
-    @users = User.paginate(:page => params[:page], :per_page => 5)
+    @users = User.paginate(:page => params[:page], :per_page => 10).order('id DESC')
   end
 
   private
