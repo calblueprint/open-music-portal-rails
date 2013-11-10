edb8fb69a4db6287b9eccc48791abab31eeec4d3
chenhoward <howardxchen@gmail.com>
2013-11-09 18:24:02 -0800

    Now only the correct user can update their own stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController 
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -53,5 +54,10 @@ class UsersController < ApplicationController
 
     def signed_in_user
       redirect_to new_session_path, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
