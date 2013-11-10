b48c614c4dcec41556493447e6dc72e59d0a7086
Mark Miyashita <negativetwelve@gmail.com>
2013-11-09 18:35:07 -0800

    adding events#users method

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -7,6 +7,12 @@ module Api
         @events = Event.all
         render json: {events: Event.to_json(@events)}
       end
+
+      def users
+        @event = Event.find(params[:event_id])
+        render json: {users: User.to_json(@event.users)}
+      end
+
     end
   end
 end
