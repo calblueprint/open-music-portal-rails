2cbb385498d4b5a55629c8a5e32b4c884a0586ae
chenhoward <howardxchen@gmail.com>
2013-11-09 17:34:00 -0800

    Front-end now actually updates users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
