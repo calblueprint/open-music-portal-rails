5ab2b59379a5121b12f372f836b60951ba899b3d
Mark Miyashita <negativetwelve@gmail.com>
2013-11-13 00:37:53 -0800

    finally got the schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131113052026) do
+ActiveRecord::Schema.define(version: 20131113083644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,12 +38,6 @@ ActiveRecord::Schema.define(version: 20131113052026) do
 
   create_table "books", force: true do |t|
     t.text     "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "categories", force: true do |t|
-    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
