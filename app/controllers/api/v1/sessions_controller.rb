bd8ad3c012177564633c44a2ac6d36db72179ac5
Mark Miyashita <negativetwelve@gmail.com>
2013-12-01 21:56:58 -0800

    Fixing login endpoint for the api

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,15 +3,14 @@ class Api::V1::SessionsController < Api::V1::BaseController
   skip_before_filter :verify_authenticity_token, only: [:login]
 
   def login
-    puts params
     email = decrypt(params[:email])
     password = decrypt(params[:password])
 
     user = User.find_by(email: email.downcase)
     if user && user.authenticate(password)
-      render json: user.to_json
+      render json: {user: user.to_json}
     else
-      render json: Response.unauthorized(user.errors.messages)
+      render json: Response.unauthorized(user)
     end
   end
 
