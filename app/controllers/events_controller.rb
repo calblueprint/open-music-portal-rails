141b996f09e9fc28501b958b789b340c17589f97
Howard Chen <howardxchen@gmail.com>
2013-12-03 09:36:12 -0800

    Edit form updates event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,9 +25,20 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(event_params)
+      flash[:success] = "Event updated"
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
   private
 
     def event_params
       params.require(:event).permit(:name)
     end
+
 end
