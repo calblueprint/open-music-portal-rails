ca5db1dc2a388075ac024dca68bc1cb60d5d71b2
Howard Chen <howardxchen@gmail.com>
2013-12-05 01:35:48 -0800

    Number of pieces shows up on event page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -39,7 +39,7 @@ class EventsController < ApplicationController
   private
 
     def event_params
-      params.require(:event).permit(:name)
+      params.require(:event).permit(:name, :num_pieces)
     end
 
     def admin_user
