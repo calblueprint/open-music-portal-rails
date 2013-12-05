36d6052af076d4641aceb86399eced4574c640d9
Howard Chen <howardxchen@gmail.com>
2013-12-05 02:23:53 -0800

    Permit max time

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -39,7 +39,7 @@ class EventsController < ApplicationController
   private
 
     def event_params
-      params.require(:event).permit(:name, :num_pieces)
+      params.require(:event).permit(:name, :num_pieces, :max_time)
     end
 
     def admin_user
