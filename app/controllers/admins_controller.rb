30838ac23cdcce008b9367767142e4d63f1b06be
Mark Miyashita <negativetwelve@gmail.com>
2013-12-05 01:53:33 -0800

    Using scope access

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -23,7 +23,7 @@ class AdminsController < ApplicationController
   private
 
     def admin_user
-      redirect_to(root_url) unless current_user != nil and current_user.has_role? :admin
+      redirect_to(root_url) unless current_user and current_user.has_role? :admin
     end
 
     def user_params
