e7d90aff56780117690520d881bf866d697af212
Jonathan Ho <jonathanho@berkeley.edu>
2013-12-10 17:09:18 -0800

    Add link to category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,11 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+
+    @user = nil
+    if signed_in?
+      @user = current_user
+    end
   end
 
 end
