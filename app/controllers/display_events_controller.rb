a43ea82f36226d0e74e55a5f85b66dd348b70e5f
Howard Chen <howardxchen@gmail.com>
2013-12-13 22:53:20 -0800

    Remove extra functions from display events controller

--- app/controllers/display_events_controller.rb
+++ app/controllers/display_events_controller.rb
@@ -1,18 +1,5 @@
 class DisplayEventsController < ApplicationController
 
-  def new
-    @display_event = DisplayEvent.new
-  end
-
-  def create
-    @display_event = DisplayEvent.new(display_event_params)
-    if @display_event.save
-      redirect_to @display_event
-    else
-      render 'new'
-    end
-  end
-
   def index
     @display_events = DisplayEvent.paginate(:page => params[:page], :per_page => 10).order('id ASC')
   end
@@ -25,26 +12,5 @@ class DisplayEventsController < ApplicationController
       @user = current_user
     end
   end
-
-  def edit
-    @display_event = DisplayEvent.find(params[:id])
-  end
-
-  def update
-    @display_event = DisplayEvent.find(params[:id])
-    if @display_event.update_attributes(display_event_params)
-      flash[:success] = "Event updated"
-      redirect_to @display_event
-    else
-      render 'new'
-    end
-  end
-
-
-  private
-
-    def display_event_params
-      params.require(:event).permit(:name, :num_pieces, :max_time)
-    end
 
 end
