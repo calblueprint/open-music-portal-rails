0791d85270d078d5e6f677d4d5819c3436e2be49
Howard Chen <howardxchen@gmail.com>
2013-12-13 18:22:20 -0800

    Fix admin index page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,31 @@
-  class Admin::UsersController < Admin::BaseController
+class Admin::UsersController < Admin::BaseController
 
     def show
       @user = User.find(params[:id])
     end
-  end
+
+    def index
+      @users = User.paginate(:page => params[:page], :per_page => 10).order('id ASC')
+    end
+
+    def edit
+      @events = Event.all
+      @user = User.find(params[:d])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      @user.attributes = user_params
+      @user.save(validate:false)
+      redirect_to @user
+    end
+
+  private
+
+    def  user_params
+      params.require(:user).permit(:first_name, :last_name, :email,
+                                     :date_of_birth, :country, :street_address, :city, :state, :zip_code,
+                                     :phone_number, :event_ids => [])
+    end
+
+end
