1429837e18dc8563570f15095cddd52e01b3448c
Mark Miyashita <negativetwelve@gmail.com>
2013-12-24 11:48:51 -0800

    Finally removing hstore from my config settings

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20131222070157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "admins", force: true do |t|
     t.datetime "created_at"
