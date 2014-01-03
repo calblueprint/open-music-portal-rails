691ac8fb32857afaecec425216244bab2585497b
Mark Miyashita <negativetwelve@gmail.com>
2014-01-02 17:17:56 -0700

    Adding admin_user helper function to the base controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,10 @@
 class Admin::BaseController < ApplicationController
   before_action :is_admin?
 
+  private
+
+    def admin_user
+      redirect_to(root_url) unless current_user and current_user.has_role? :admin
+    end
+
 end
